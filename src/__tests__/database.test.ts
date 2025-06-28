00eb29801b5a46cc55289015f31353f8e472782f
davidasix <david@davidasix.com>
2025-06-28 11:31:33 -0400

    Change check order, add a db test

--- src/__tests__/database.test.ts
+++ src/__tests__/database.test.ts
@@ -40,3 +40,11 @@
 //     }
 //   });
 // });
+
+import { describe, it, expect } from "vitest";
+
+describe("Dummy Database Test", () => {
+  it("should always pass", () => {
+    expect(true).toBe(true);
+  });
+});
