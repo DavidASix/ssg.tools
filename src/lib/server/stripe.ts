4fdad224081eb55af7e82146c26df1abc6112493
davidasix <david@davidasix.com>
2025-07-05 01:24:48 -0400

    Error guard for Stripe

--- src/lib/server/stripe.ts
+++ src/lib/server/stripe.ts
@@ -2,6 +2,14 @@ import "server-only";
 
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+
+// Guard check added to avoid failing builds which don't have the environment variable set
+if (!stripeSecretKey) {
+  console.error("STRIPE_SECRET_KEY is not set in the environment variables.");
+  throw new Error("Stripe environment variable is required");
+}
+
+export const stripe = new Stripe(stripeSecretKey, {
   apiVersion: "2025-04-30.basil",
 });
