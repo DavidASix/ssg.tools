4f49864d5b670dcbd8d2076a15285eaeb072fac3
davidasix <david@davidasix.com>
2025-07-05 01:29:49 -0400

    Testing fake key

--- src/lib/server/stripe.ts
+++ src/lib/server/stripe.ts
@@ -2,13 +2,7 @@ import "server-only";
 
 import Stripe from "stripe";
 
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
-
-// Guard check added to avoid failing builds which don't have the environment variable set
-if (!stripeSecretKey) {
-  console.error("STRIPE_SECRET_KEY is not set in the environment variables.");
-  throw new Error("Stripe environment variable is required");
-}
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY || "sk_test_fake";
 
 export const stripe = new Stripe(stripeSecretKey, {
   apiVersion: "2025-04-30.basil",
