a31a16b79893ed0269f033066797c867bf14ad73
davidasix <david@davidasix.com>
2025-07-05 01:40:09 -0400

    Added JS Doc

--- src/lib/server/stripe.ts
+++ src/lib/server/stripe.ts
@@ -2,6 +2,10 @@ import "server-only";
 
 import Stripe from "stripe";
 
+/**
+ * A fake key is provided for fallback as during the build process the env variable
+ * is not available, but stripe throws a build-breaking error if it cannot initialize
+ */
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY || "sk_test_fake";
 
 export const stripe = new Stripe(stripeSecretKey, {
