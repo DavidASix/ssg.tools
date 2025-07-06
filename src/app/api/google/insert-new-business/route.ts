c25ac923b2d697e8aa14d7fc1ee683a94b3407c8
davidasix <david@davidasix.com>
2025-07-06 12:04:56 -0400

    Endpoint to set up initial business

---
+++ src/app/api/google/insert-new-business/route.ts
@@ -0,0 +1,61 @@
+import { NextResponse } from "next/server";
+
+import schema from "./schema";
+import { NextRouteContext, RequestHandler } from "@/middleware/types";
+import { withAuth } from "@/middleware/withAuth";
+import { withBody } from "@/middleware/withBody";
+
+import {
+  updateBusinessReviews,
+  updateBusinessStats,
+} from "@/lib/server/google/update";
+import { recordEvent } from "@/lib/server/events";
+import { userHasOwnership } from "@/lib/ownership";
+import { businesses } from "@/schema/schema";
+import { db } from "@/schema/db";
+
+/**
+ * This endpoint inserts a new business into the database using the provided Google Place ID.
+ * It then fetches the initial reviews and business stats for that business.
+ */
+export const POST: RequestHandler<NextRouteContext> = withAuth(
+  withBody(schema, async (_, context) => {
+    try {
+      const { place_id, name, formatted_address } = context.body;
+      const [business] = await db
+        .insert(businesses)
+        .values({
+          place_id,
+          name: name || null,
+          address: formatted_address || null,
+          user_id: context.user_id,
+        })
+        .returning({ id: businesses.id });
+
+      await userHasOwnership(context.user_id, business.id, businesses);
+
+      const insertedStats = await updateBusinessStats(business.id);
+      const insertedReviews = await updateBusinessReviews(business.id);
+      await recordEvent("update_reviews", context.user_id, {
+        business_id: business.id,
+      });
+      await recordEvent("update_stats", context.user_id, {
+        business_id: business.id,
+      });
+
+      const response = schema.response.parse({
+        business_id: business.id,
+        reviews: insertedReviews,
+        stats: insertedStats,
+      });
+
+      return NextResponse.json(response, { status: 200 });
+    } catch (error) {
+      console.error("Error processing request:", error);
+      return NextResponse.json(
+        { error: "Internal Server Error" },
+        { status: 500 },
+      );
+    }
+  }),
+);
---
+++ src/app/api/google/insert-new-business/schema.ts
@@ -0,0 +1,30 @@
+import { z } from "zod";
+import type { APISchema } from "@/schema/types";
+
+const schema = {
+  url: "/api/google/insert-new-business",
+  request: z.object({
+    place_id: z.string(),
+    name: z.string().optional(),
+    formatted_address: z.string().optional(),
+  }),
+  response: z.object({
+    business_id: z.number(),
+    reviews: z
+      .object({
+        author_name: z.string().nullable(),
+        author_image: z.string().nullable(),
+        datetime: z.string().nullable(),
+        link: z.string().nullable(),
+        rating: z.number().nullable(),
+        comments: z.string().nullable(),
+      })
+      .array(),
+    stats: z.object({
+      review_count: z.number().nullable(),
+      review_score: z.number().nullable(),
+    }),
+  }),
+} satisfies APISchema;
+
+export default schema;
