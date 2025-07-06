e9dfb7f85e48a001e4a29808aa699e7454f96d5a
davidasix <david@davidasix.com>
2025-07-06 13:47:32 -0400

    Moved stringdate type to centralized point

--- src/app/api/google/insert-new-business/schema.ts
+++ src/app/api/google/insert-new-business/schema.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import type { APISchema } from "@/schema/types";
+import { stringDate, type APISchema } from "@/schema/types";
 
 const schema = {
   url: "/api/google/insert-new-business",
@@ -14,7 +14,7 @@ const schema = {
       .object({
         author_name: z.string().nullable(),
         author_image: z.string().nullable(),
-        datetime: z.string().nullable(),
+        datetime: stringDate.nullable(),
         link: z.string().nullable(),
         rating: z.number().nullable(),
         comments: z.string().nullable(),
